9571bbe24e1bb175a9fa6dc9307d2d388f862aea
Spencer Evans <spencevans@gmail.com>
2019-06-26 22:16:02 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push "arrays!"
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift "wow"
+  array.unshift(element) 
 end
 
 def remove_element_from_end_of_array(array)
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[2]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
